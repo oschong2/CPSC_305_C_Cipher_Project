aef524669264213971e63d2cf2be496a3b5885dc
1DianaGuzman1 <101741525+1DianaGuzman1@users.noreply.github.com>
2022-10-23 18:59:42 -0400

    Update augustus.h

--- augustus.h
+++ augustus.h
@@ -3,5 +3,6 @@
 
 char *augustus_encrypt(char *plain, char *key);
 char *augustus_decrypt(char *cipher, char *key);
-
+char Augustus_encrypt_char(char plain, int key);
+char *Augustus_encrypt(char *plain, char *key);
 #endif //_AUGUSTUS_H
